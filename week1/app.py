09d41673454993f7424783c4a1e09d326b21f9a5
Dikshit <dikshit19csu379@ncuindia.edu>
2021-01-05 20:57:17 -0800

    basic dev

--- week1/app.py
+++ week1/app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, flash, redirect
+from wtforms import Form, TextAreaField, TextField, validators, SubmitField
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def helloWorld():
-    return'<h1>Hello!</h1>'
+@app.route('/', methods=["GET", "POST"])
+@app.route('/home', methods=["GET", "POST"])
+class ReviewForm(Form):
+    fname = TextField('First Name*', validators=[validators.DataRequired()])
+    product = TextField(
+        'Product Name*', validators=[validators.DataRequired()])
+    review = TextAreaField('Review*', validators=[validators.DataRequired()])
+    submit = SubmitField('Publish Review')
+
+
+def review():
+    form = ReviewForm()
+    if form.validate_on_submit():
+        flash('Review Sumbitted', 'success')
+        return redirect(url_for('home'))
+    else:
+        flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('index.html', form=form)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
