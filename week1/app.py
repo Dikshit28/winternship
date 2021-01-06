648d6e399387f51f4308a4620e54418fe54ba564
Dikshit <dikshit19csu379@ncuindia.edu>
2021-01-06 00:05:21 -0800

    FIXING

--- week1/app.py
+++ week1/app.py
@@ -1,26 +1,29 @@
 from flask import Flask, render_template, url_for, flash, redirect
-from wtforms import Form, TextAreaField, TextField, validators, SubmitField
+from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
+from wtforms import Form, TextAreaField, TextField, SubmitField, StringField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 
-@app.route('/', methods=["GET", "POST"])
-@app.route('/home', methods=["GET", "POST"])
-class ReviewForm(Form):
-    fname = TextField('First Name*', validators=[validators.DataRequired()])
-    product = TextField(
-        'Product Name*', validators=[validators.DataRequired()])
-    review = TextAreaField('Review*', validators=[validators.DataRequired()])
+class ReviewForm(FlaskForm):
+    fname = StringField('First Name', validators=[DataRequired()])
+    product = StringField(
+        'Product Name', validators=[DataRequired()])
+    review = TextAreaField('Review', validators=[DataRequired()])
     submit = SubmitField('Publish Review')
 
 
+@app.route('/', methods=["GET", "POST"])
+@app.route('/index', methods=["GET", "POST"])
 def review():
     form = ReviewForm()
     if form.validate_on_submit():
         flash('Review Sumbitted', 'success')
-        return redirect(url_for('home'))
+        return redirect(url_for('review'))
     else:
-        flash('Login Unsuccessful. Please check username and password', 'danger')
+        flash('Unsuccessful. Please check again', 'danger')
     return render_template('index.html', form=form)
 
 
