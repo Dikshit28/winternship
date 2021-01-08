f6748da7d12fcbdfff70a507aa5e6484e2e5d6f9
Dikshit <dikshit19csu379@ncuindia.edu>
2021-01-07 23:23:06 -0800

    fixed

--- week1/app.py
+++ week1/app.py
@@ -1,40 +1,36 @@
 from flask import Flask, render_template, request
-
+import csv
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-posts = [
-    {
-        'Name': 'abcd',
-        'Product Name': 'cooler',
-        'Review': 'nice'
-    },
-    {
-        'Name': 'efgh',
-        'Product Name': 'heater',
-        'Review': 'lmao'
-    },
-    {
-        'Name': 'ijkl',
-        'Product Name': 'machine',
-        'Review': 'lol'
-    }
-]
+
+posts = []
 
 
 @app.route('/', methods=["GET", "POST"])
 @app.route('/index', methods=["GET", "POST"])
 def home():
+    # DATABASE INITIALIZE
+    database_read = open("week1/database/reviews.csv", "r", newline='')
+    database_write = open("week1/database/reviews.csv", "a", newline='')
+    reader_obj = csv.reader(database_read)
+    writer_obj = csv.writer(database_write)
+    # DATABASE INITIALIZED
+
     if request.method == "POST":
         print(request.form)
-        posts.insert(
-            0,
-            {
-                'Name': request.form.get('name'),
-                'Product Name': request.form.get('product'),
-                'Review': request.form.get('review')
-            }
+        writer_obj.writerow(
+            (
+                request.form.get('name'),
+                request.form.get('product'),
+                request.form.get('review')
+            )
         )
+        database_write.close()
+        for row in reader_obj:
+            print(row)
+            if row not in posts:
+                posts.insert(0, row)
     return render_template('index.jinja2', reviews=posts)
 
 
